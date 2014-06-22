1761be4e79feb2d23d8c5ac6d7d1b9f207f56c88
Ashwin-Praveen <ashwinpraveen1@gmail.com>
2014-06-23 00:05:35 +0530

    Exchange function!

--- Exchange.c
+++ Exchange.c
@@ -1,25 +1,5 @@
 #include<stdio.h>
-
-int Exchange(int *A, int *B);
-
-int main()
-{
-
-	int a,b;
-
-	printf("Enter two numbers to be swapped respectivety:\n");
-	printf("A:");
-	scanf("%d",&a);
-	printf("B:");
-	scanf("%d",&b);
-
-	Exchange(&a,&b);
-	
-	printf("\n\n\nA:%d\nB:%d\n\n",a,b);
-
-	return 0;
-
-}
+#include<stdlib.h>
 
 int Exchange(int *A, int *B)
 {
